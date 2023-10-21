f468ddd0b9b9969b175d8038347c1d9e00db0c55
MinhPhan0104 <87423834+MinhPhan0104@users.noreply.github.com>
2023-10-21 16:12:35 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo cp ../node_modules.zip .
 sudo unzip node_modules.zip
-ts-node index.ts > stdout.txt 2> stderr.txt &
+/home/meomeo_development/.nvm/versions/node/v18.18.2/bin/ts-node index.ts > stdout.txt 2> stderr.txt &
