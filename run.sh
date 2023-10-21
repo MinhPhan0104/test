d00bf77288d5be5ad8e59b3f8264867eadb6f3a0
MinhPhan0104 <87423834+MinhPhan0104@users.noreply.github.com>
2023-10-21 16:56:22 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#! /bin/bash
+cd /home
+sudo rm -rf ws-test
+sudo git clone https://github.com/MinhPhan0104/ws-test.git
+sudo chmod 777 ws-test/
+cd ws-test/
+sudo chmod +x run.sh
 cp -r node_modules /home/ws-test/
 sudo cd /home/ws-test
 npm start
